74d25bfb1d8b7ca027c875ad1e0f32cfd3898627
Danny Fung <dannyfung97@gmail.com>
2019-02-14 13:42:49 -0800

    ajax and facebook lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -20,11 +20,17 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
-
+	
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+	$.get("/project/"+idNumber, callback);
 
 	console.log("User clicked on project " + idNumber);
+}
+
+function callback(res){
+	console.log(res);
+	$("#project"+res.id+" .details").html(res.summary);
 }
